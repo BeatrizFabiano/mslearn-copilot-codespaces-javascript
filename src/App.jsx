5219369c141e459085cbfacd487ddb069d0992df
BEATRIZ  FABIANO <beatrizfabiano81@gmail.com>
2023-10-10 12:34:23 +0000

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Beatriz Fabiano",
+  title: "Web Developer & Dynamics 365 student",
+  email: "beatriz@example.com",
+  gitHub: "beatriz",
+  instagram: "beatriz",
+  linkedIn: "beatriz",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "beatriz",
+  youTube: "beatriz",
 };
 
 const primaryColor = "#4E567E";
